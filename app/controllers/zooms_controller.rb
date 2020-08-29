63362b557d66f81e2ab1e41ed1d8333c986a0119
kaz-i-54 <torune.summer@gmail.com>
2020-08-29 22:44:27 +0900

    シェフのお勧め度

--- app/controllers/zooms_controller.rb
+++ app/controllers/zooms_controller.rb
@@ -1,5 +1,11 @@
 RAND_GET_NUMBER = 3
 
+BEST_SCORE = 10
+SECOND_SCORE = 5
+THIRD_SCORE = 3
+FORTH_SCORE = 2
+WORST_SCORE = 1
+
 class ZoomsController < ApplicationController
 
     def index
@@ -50,4 +56,26 @@ class ZoomsController < ApplicationController
     def zoom_params
         params.require(:zoom).permit(:text)
     end
+
+
+    def recipe_rank(recipe)
+        if recipe
+            case recipe.frequency
+            when 0..WORST_SCORE
+                "-"
+            when WORST_SCORE+1..FORTH_SCORE
+                "★"*1 
+            when FORTH_SCORE+1..THIRD_SCORE
+                "★"*2
+            when THIRD_SCORE+1..SECOND_SCORE
+                "★"*3 
+            when SECOND_SCORE+1..BEST_SCORE
+                "★"*4 
+            else
+                "★"*5
+            end
+        end
+    end
+
+    helper_method :recipe_rank
 end
